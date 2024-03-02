d471800ad70f9fb82ba4488f8e179c6acec40134
Dr Bill Close <90579801+bill-close@users.noreply.github.com>
2024-03-02 16:31:51 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from shiny import render, ui
 from shiny.express import input
+from palmerpenguins import load_penguins
+import seaborn as sns
 
-ui.panel_title("Hello Shiny!")
-ui.input_slider("n", "N", 0, 100, 20)
+ui.panel_title("A plot of the flipper length of penguins from various species")
+# ui.input_slider("n", "N", 0, 100, 20)
+
+# @render.text
+# def txt():
+#     return f"n*2 is {input.n() * 2}"
 
 
-@render.text
-def txt():
-    return f"n*2 is {input.n() * 2}"
+@render.plot(alt="A plot of the penguins")
+def plot():
+    sns.set_style("whitegrid")
 
+    penguins = load_penguins()
 
+    g = sns.histplot(
+        x="flipper_length_mm",
+        hue="species",
+        data=penguins,
+        palette=["#FF8C00","#159090","#FF6347"],
+        linewidth=0.3,
+    )
+    return g
